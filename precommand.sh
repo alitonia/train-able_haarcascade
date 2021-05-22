175b739a97d07f632258dd02f7afadba04bd3d58
Zen-net <allyofjustice1@gmail.com>
2021-05-22 12:04:35 +0700

    more clean up

--- precommand.sh
+++ precommand.sh
@@ -6,9 +6,9 @@ printf "change opencv to 3.4\n"
 cd opencv && git checkout 3.4 && cd ..
 
 printf "build from source\n"
-cd opencv
+cd opencv || exit
 mkdir -p build
-cd build
+cd build || exit
 
 cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=/usr/local ..
 
