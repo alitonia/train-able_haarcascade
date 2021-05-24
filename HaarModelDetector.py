2a92fb97fc36d02f0d5b38f47731b90209889f74
ToastedPeanut <leehoang2000@gmail.com>
2021-05-24 21:55:23 +0700

    add conf to detector returnlist

--- HaarModelDetector.py
+++ HaarModelDetector.py
@@ -4,10 +4,23 @@ import cv2
 from matplotlib import pyplot as plt
 
 class ModelDetector:
+    face_cascade = None
     def prepare(self):
         self.face_cascade = cv2.CascadeClassifier('model/cascade.xml')
 
     # Defining a function that will do the detections
     def detect(self, frame):
-        return self.face_cascade.detectMultiScale(frame, 1.3, 5)
+        detected = self.face_cascade.detectMultiScale3(frame, 1.3, 5, outputRejectLevels = True)
+        rects = detected[0]
+        weights = detected[2]
+        conf = weights[:,0]/max(weights[:,0])
         
+        returnList = []
+        for i in range(conf.size):
+            x,y,w,h = rects[i]
+            returnList.append([conf[i],x,y,w,h])
+        return returnList
+    
+    def detect_with_path(self, path_to_image):
+        frame = cv2.imread(path_to_image)
+        return self.detect(frame)
--- testDetector.py
+++ testDetector.py
@@ -2,7 +2,7 @@
 import os
 import cv2
 from matplotlib import pyplot as plt
-import ModelDetector as md
+import HaarModelDetector as md
 
 #Prepare model
 detector = md.ModelDetector()
@@ -10,12 +10,7 @@ detector.prepare()
 
 #Use model to predict 
 test_image_path = 'anh_hoi_dong.jpg'
-img = cv2.imread(test_image_path)
-detected = detector.detect(img)
+detected = detector.detect_with_path(test_image_path)
 
-#Write to new file named "resultbb.txt"
-file_path = "resultbb.txt"
-with open(file_path, 'w') as file:
-    for (x,y,w,h) in detected:
-        file.write(str(x) + ' ' + str(y) + ' ' + str(w) + ' ' + str(h) + '\n')
-        
+for (conf, x,y,w,h) in detected:
+    print(conf, x,y,w,h)
